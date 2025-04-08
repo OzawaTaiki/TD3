5eb38a99215f58d1e4c16a4f8c47093c8caf3a33
nakanuma <k023g0121@g.neec.ac.jp>
2025-04-09 00:26:46 +0900

    更新

--- TD3_1/Src/Application/Tower/Tower.h
+++ TD3_1/Src/Application/Tower/Tower.h
@@ -28,7 +28,7 @@ private:
 	uint32_t texture_;
 	std::unique_ptr<AABBCollider> collider_;
 
-	uint32_t hp_ = 10;
+	uint32_t hp_ = 10; 
 
 	// 敵と1度だけ衝突判定を行うよう、処理済みコライダーを追加
 	std::unordered_set<Collider*> processedColliders_;
