79c3a57764133bbb186f72c5d9462d46320fcec8
nakanuma <k023g0121@g.neec.ac.jp>
2025-04-08 19:25:58 +0900

    オブジェクトの追加 ・ テクスチャ変更

--- TD3_1/Src/Application/UI/Game/GameUI.cpp
+++ TD3_1/Src/Application/UI/Game/GameUI.cpp
@@ -6,24 +6,24 @@
 void GameUI::Initialize()
 {
 	// 操作説明（文字）
-	uint32_t textureTextGuide = TextureManager::GetInstance()->Load("game/textGuide.png");
+	uint32_t textureTextGuide = TextureManager::GetInstance()->Load("game/ui/textGuide.png");
 	spriteTextGuide_ = Sprite::Create("textGuide", textureTextGuide);
 	spriteTextGuide_->Initialize();
 	spriteTextGuide_->translate_ = { 250, 85 };
 
 	// 打ち上げゲージ（文字）
-	uint32_t textureTextGauge = TextureManager::GetInstance()->Load("game/textGauge.png");
+	uint32_t textureTextGauge = TextureManager::GetInstance()->Load("game/ui/textGauge.png");
 	spriteTextGauge_ = Sprite::Create("textGauge", textureTextGauge);
 	spriteTextGauge_->Initialize();
 	spriteTextGauge_->translate_ = { 1015, 45 };
 
 	//// 打ち上げゲージ（内側 : ゲージ本体）
-	//uint32_t textureGauge = TextureManager::GetInstance()->Load("game/gauge.png");
+	//uint32_t textureGauge = TextureManager::GetInstance()->Load("game/ui/gauge.png");
 	//spriteGauge_ = Sprite::Create("gauge", textureGauge);
 	//spriteGauge_->translate_ = { 1015, 125 };
 
 	//// 打ち上げゲージ（外側 : 枠）
-	//uint32_t textureGaugeFrame = TextureManager::GetInstance()->Load("game/gaugeFrame.png");
+	//uint32_t textureGaugeFrame = TextureManager::GetInstance()->Load("game/ui/gaugeFrame.png");
 	//spriteGaugeFrame_ = Sprite::Create("gaugeFrame", textureGaugeFrame);
 	//spriteGaugeFrame_->translate_ = { 1015, 125 };
 }
