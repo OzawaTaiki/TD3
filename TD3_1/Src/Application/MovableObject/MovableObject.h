f071c7399fa09f0576541b8c8a87c3b862261cee
nakanuma <k023g0121@g.neec.ac.jp>
2025-04-09 12:31:58 +0900

    MovableObjectクラスの作成

---
+++ TD3_1/Src/Application/MovableObject/MovableObject.h
@@ -0,0 +1,38 @@
+#pragma once
+
+// Engine
+#include <Features/Model/ObjectModel.h>
+#include <Features/Collision/Manager/CollisionManager.h>
+
+class MovableObject {
+public:
+	virtual ~MovableObject() = default;
+
+	virtual void Initialize() = 0;
+	virtual void Update() = 0;
+	virtual void Draw(const Camera& camera) = 0;
+
+protected:
+	std::unique_ptr<ObjectModel> object_;
+	std::unique_ptr<AABBCollider> collider_;
+
+	uint32_t texture_;
+};
+
+/// <summary>
+/// 箱型オブジェクト
+/// </summary>
+class BoxObject : public MovableObject {
+public:
+	void Initialize() override;
+	void Update() override;
+	void Draw(const Camera& camera) override;
+};
+
+/// <summary>
+/// 筒型オブジェクト
+/// </summary>
+class CylinderObject : public MovableObject {
+public:
+
+};
